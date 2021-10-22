2e27dc021c8d310a40e9c90f4cce14631ea7129a
Stewart Lynch <slynch@createchsol.com>
2021-10-22 16:51:38 -0700

    Added constraint error removal.

--- Firebase Sample/Firebase_SampleApp.swift
+++ Firebase Sample/Firebase_SampleApp.swift
@@ -16,6 +16,9 @@ struct Firebase_SampleApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onAppear {
+                    UserDefaults.standard.setValue(false, forKey: "_UIConstraintBasedLayoutLogUnsatisfiable")
+                }
         }
     }
 }
